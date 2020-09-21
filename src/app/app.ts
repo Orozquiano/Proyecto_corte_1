e77f0163314940b3b7b5148c31c8746165c62572
Carlos Andres Ortega Yate <71539286+CarlosAndresOrtega@users.noreply.github.com>
2020-09-21 14:27:30 -0500

    Add files via upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,33 @@
 import { count } from './counter'
 import { HomePageController } from './pages/Home/HomePageController'
+import { HabilidadesPageController } from './pages/HabilidadesPage/HabilidadesPageController'
 class App {
   
   mainPages = [
-    HomePageController
+    HomePageController,
+    HabilidadesPageController
   ];
+  loaded: any =[]; 
 
   render() {
     const component = this.component();
+
     this.mainPages.forEach(pageController => {
-    
+      const controller = new pageController();
+      this.loaded.push(controller);
       const [elId, element] = new pageController().getView();
       component.appendChild(this.createPage(elId, element));
     })
-    
+
     document.body.append(component);
+    /**
+     * Este es el cambio
+     */
+    document.body.onload = () =>{
+      this.loaded.forEach((controller: any) => {
+        controller.start();
+      })
+    }
   }
 
   createPage(id: string, element: DocumentFragment) {
